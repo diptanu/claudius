5af4637d7ea96559b1b6bbacbb63ecf6638bf1ff
Diptanu Choudhury <diptanuc@gmail.com>
2014-06-13 00:11:17 -0700

    Implemented tailing the log file

--- main.go
+++ main.go
@@ -1,25 +1,48 @@
 package main
 
-import(
-    "fmt"
-    "github.com/julienschmidt/httprouter"
-    "net/http"
-    "log"
+import (
+	"flag"
+	"fmt"
+	"github.com/ActiveState/tail"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
 )
 
+var port = flag.Int("http-port", 24987, "Http Port for claudius")
 
 func Ping(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-    fmt.Fprint(w, "ping")
+	fmt.Fprint(w, "ping")
 }
 
 func Log(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-    fmt.Fprintf(w, "Logs for %s", ps.ByName("containerId"))
+	containerId := ps.ByName("containerId")
+	fmt.Fprintf(w, "Logs for %s", containerId)
 }
 
+func Tail(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	containerId := ps.ByName("containerId")
+	logFilePath := fmt.Sprintf("/var/lib/docker/containers/%s/%s-json.log", containerId, containerId)
+	hj := w.(http.Hijacker)
+	conn, bufrw, err := hj.Hijack()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer conn.Close()
+	t, err := tail.TailFile(logFilePath, tail.Config{Follow: true})
+	for line := range t.Lines {
+		bufrw.WriteString(fmt.Sprintf("%s\n", line.Text))
+		bufrw.Flush()
+	}
+}
 
 func main() {
-    router := httprouter.New()
-    router.GET("/ping", Ping)
-    router.GET("/logs/:containerId", Log)
-    log.Fatal(http.ListenAndServe(":8080", router))
+	flag.Parse()
+	httpPort := fmt.Sprintf(":%d", *port)
+	router := httprouter.New()
+	router.GET("/ping", Ping)
+	router.GET("/logs/:containerId", Log)
+	router.GET("/tail/:containerId", Tail)
+	log.Fatal(http.ListenAndServe(httpPort, router))
 }
